667687fbf5a2115c44362d9ec50edee38be8ebd3
unknown <macjay314@gmail.com>
2020-06-25 11:32:20 -0700

    First attempt to check for 3 lines

--- src/thing.js
+++ src/thing.js
@@ -23,6 +23,9 @@ const changeState = (prop) => {
 const feed = changeState("haiku");
 const blueFood = changeState("soil")(5);
 
+console.log(blueFood);
+
+
 // $(document).ready(function() {
 
 // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect.
@@ -31,3 +34,19 @@ const blueFood = changeState("soil")(5);
 //     $('#soil-value').text(newState.soil);
 //   });
 // });
+
+
+// Your logic should verify that the poem has three lines.
+// Your logic should verify English syllable rules (and exceptions) one at a time. A quick Google search will provide information on English syllable rules.
+// If you successfully complete a Haiku checker, continue to build out your application to randomly generate haikus.
+
+// I am first with five
+// Then seven in the middle
+// Five again to end.
+
+function isHaiku(poem){
+  return poem.length = 3;
+}
+
+const testPoem = ["I am first with five", "Then seven in the middle", "Five again to end."];
+const haikuTest = isHaiku(testPoem);
